1451fa877f6cc1ea89265dd96c548a7a125d3ffe
larkin <larkin.qubership@gmail.com>
2025-08-14 10:16:25 +0400

    Initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/taurmorchant/go-test-service-a
+
+go 1.24.1
---
+++ printer.go
@@ -0,0 +1,7 @@
+package go_test_service_a
+
+import "fmt"
+
+func Hello() {
+	fmt.Println("Hello, World from go-test-service-a")
+}
